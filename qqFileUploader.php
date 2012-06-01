cccd33f428d1e89d54e99aec694316a7b91f25c1
vovanz <vovanzagorodsky@gmail.com>
2012-06-01 19:03:15 +0600

    Добавлена транслитерация кириллический названий файлов.

--- qqFileUploader.php
+++ qqFileUploader.php
@@ -1,8 +1,11 @@
 <?php
+
 /**
  * Handle file uploads via XMLHttpRequest
  */
 class qqUploadedFileXhr {
+
+
     /**
      * Save the file to the specified path
      * @return boolean TRUE on success
@@ -13,7 +16,7 @@ class qqUploadedFileXhr {
         $realSize = stream_copy_to_stream($input, $temp);
         fclose($input);
 
-        if ($realSize != $this->getSize()){
+        if ($realSize != $this->getSize()) {
             return false;
         }
 
@@ -24,46 +27,72 @@ class qqUploadedFileXhr {
 
         return true;
     }
+
     function getName() {
         return $_GET['qqfile'];
     }
+
     function getSize() {
-        if (isset($_SERVER["CONTENT_LENGTH"])){
-            return (int)$_SERVER["CONTENT_LENGTH"];
+        if (isset($_SERVER["CONTENT_LENGTH"])) {
+            return (int) $_SERVER["CONTENT_LENGTH"];
         } else {
             throw new Exception('Getting content length is not supported.');
         }
     }
+
 }
 
 /**
  * Handle file uploads via regular form post (uses the $_FILES array)
  */
 class qqUploadedFileForm {
+
     /**
      * Save the file to the specified path
      * @return boolean TRUE on success
      */
     function save($path) {
-        if(!move_uploaded_file($_FILES['qqfile']['tmp_name'], $path)){
+        if (!move_uploaded_file($_FILES['qqfile']['tmp_name'], $path)) {
             return false;
         }
         return true;
     }
+
     function getName() {
         return $_FILES['qqfile']['name'];
     }
+
     function getSize() {
         return $_FILES['qqfile']['size'];
     }
+
 }
 
 class qqFileUploader {
+
     private $allowedExtensions = array();
     private $sizeLimit = 10485760;
     private $file;
-
-    function __construct(array $allowedExtensions = array(), $sizeLimit = 10485760){
+    
+    function translitIt($str) {
+        $tr = array(
+            "А" => "A", "Б" => "B", "В" => "V", "Г" => "G",
+            "Д" => "D", "Е" => "E", "Ж" => "J", "З" => "Z", "И" => "I",
+            "Й" => "Y", "К" => "K", "Л" => "L", "М" => "M", "Н" => "N",
+            "О" => "O", "П" => "P", "Р" => "R", "С" => "S", "Т" => "T",
+            "У" => "U", "Ф" => "F", "Х" => "H", "Ц" => "TS", "Ч" => "CH",
+            "Ш" => "SH", "Щ" => "SCH", "Ъ" => "", "Ы" => "YI", "Ь" => "",
+            "Э" => "E", "Ю" => "YU", "Я" => "YA", "а" => "a", "б" => "b",
+            "в" => "v", "г" => "g", "д" => "d", "е" => "e", "ж" => "j",
+            "з" => "z", "и" => "i", "й" => "y", "к" => "k", "л" => "l",
+            "м" => "m", "н" => "n", "о" => "o", "п" => "p", "р" => "r",
+            "с" => "s", "т" => "t", "у" => "u", "ф" => "f", "х" => "h",
+            "ц" => "ts", "ч" => "ch", "ш" => "sh", "щ" => "sch", "ъ" => "y",
+            "ы" => "yi", "ь" => "", "э" => "e", "ю" => "yu", "я" => "ya"
+        );
+        return strtr($str, $tr);
+    }
+    function __construct(array $allowedExtensions = array(), $sizeLimit = 10485760) {
         $allowedExtensions = array_map("strtolower", $allowedExtensions);
 
         $this->allowedExtensions = $allowedExtensions;
@@ -80,20 +109,20 @@ class qqFileUploader {
         }
     }
 
-    private function checkServerSettings(){
+    private function checkServerSettings() {
         $postSize = $this->toBytes(ini_get('post_max_size'));
         $uploadSize = $this->toBytes(ini_get('upload_max_filesize'));
 
-        if ($postSize < $this->sizeLimit || $uploadSize < $this->sizeLimit){
+        if ($postSize < $this->sizeLimit || $uploadSize < $this->sizeLimit) {
             $size = max(1, $this->sizeLimit / 1024 / 1024) . 'M';
             die("{'error':'increase post_max_size and upload_max_filesize to $size'}");
         }
     }
 
-    private function toBytes($str){
+    private function toBytes($str) {
         $val = trim($str);
-        $last = strtolower($str[strlen($str)-1]);
-        switch($last) {
+        $last = strtolower($str[strlen($str) - 1]);
+        switch ($last) {
             case 'g': $val *= 1024;
             case 'm': $val *= 1024;
             case 'k': $val *= 1024;
@@ -104,12 +133,12 @@ class qqFileUploader {
     /**
      * Returns array('success'=>true) or array('error'=>'error message')
      */
-    function handleUpload($uploadDirectory, $replaceOldFile = FALSE){
-        if (!is_writable($uploadDirectory)){
+    function handleUpload($uploadDirectory, $replaceOldFile = FALSE) {
+        if (!is_writable($uploadDirectory)) {
             return array('error' => "Server error. Upload directory isn't writable.");
         }
 
-        if (!$this->file){
+        if (!$this->file) {
             return array('error' => 'No files were uploaded.');
         }
 
@@ -124,28 +153,28 @@ class qqFileUploader {
         }
 
         $pathinfo = pathinfo($this->file->getName());
-        $filename = $pathinfo['filename'];
+        $filename = $this->translitIt($pathinfo['filename']);
         //$filename = md5(uniqid());
         $ext = $pathinfo['extension'];
 
-        if($this->allowedExtensions && !in_array(strtolower($ext), $this->allowedExtensions)){
+        if ($this->allowedExtensions && !in_array(strtolower($ext), $this->allowedExtensions)) {
             $these = implode(', ', $this->allowedExtensions);
-            return array('error' => 'File has an invalid extension, it should be one of '. $these . '.');
+            return array('error' => 'File has an invalid extension, it should be one of ' . $these . '.');
         }
 
-        if(!$replaceOldFile){
+        if (!$replaceOldFile) {
             /// don't overwrite previous files that were uploaded
             while (file_exists($uploadDirectory . $filename . '.' . $ext)) {
                 $filename .= rand(10, 99);
             }
         }
 
-        if ($this->file->save($uploadDirectory . $filename . '.' . $ext)){
-            return array('success'=>true,'filename'=>$filename.'.'.$ext);
+        if ($this->file->save($uploadDirectory . $filename . '.' . $ext)) {
+            return array('success' => true, 'filename' => $filename . '.' . $ext);
         } else {
-            return array('error'=> 'Could not save uploaded file.' .
+            return array('error' => 'Could not save uploaded file.' .
                 'The upload was cancelled, or server error encountered');
         }
-
     }
+
 }
